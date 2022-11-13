0933b67c550f34ca18c1e4c2a4b33f4a8d3dfc22
Alan <alaan_roodriguez@hotmail.com>
2022-11-13 17:38:10 -0300

    webpack

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mainRouter = void 0;
-const express_1 = require("express");
-const mainRouter = (0, express_1.Router)();
-exports.mainRouter = mainRouter;
-const notas_1 = require("./notas");
-mainRouter.use('/notas', notas_1.router);
