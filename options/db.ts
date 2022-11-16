214bc6397c3d78769e7fbc8e0dd0f0da0bed5250
Alan <alaan_roodriguez@hotmail.com>
2022-11-16 00:11:28 -0300

    knex/mysql

--- options/db.ts
+++ options/db.ts
@@ -1,12 +1,17 @@
-const knex = require('knex')({
-    client: 'mysql',
+const knex = {
+    client: 'mysql2',
     connection: {
       host : '127.0.0.1',
       port : 3306,
       user : 'root',
+      /* HACER QUE LA PASSWORD NO PUEDA SER VISIBLE PARA NADIE:
+        - ignorar en git
+        - hacer que npm run build NO COMPILE esta carpeta
+        - o si las dos anteriores opciones no funcionan, descargar XAMPP (seguramente haya que eliminar SQL instalado)
+      */
       password : '',
-      database : '`nodejs_coderhouse`'
+      database : 'nodejs_coderhouse'
     }
-  });
+  };
 
   export default knex;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
    mode: 'production',
-   entry: './src/main.ts',
+   entry: './src/index.ts',
    target: "node",
    externals: [nodeExternals()],
 
